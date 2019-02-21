2424bd2cb6e33f3c2582a46aa6fd938d9ae5ec5d
Nuno Miguel <37627936+nunomiguel22@users.noreply.github.com>
2019-02-21 12:16:14 +0000

    Update MyParallelogram.js

--- EX1/MyParallelogram.js
+++ EX1/MyParallelogram.js
@@ -11,23 +11,18 @@ class MyParallelogram extends CGFobject {
 	initBuffers() {
 		this.vertices = [
 			0, 0, 0,	//0
-			1, 1, 0,	//1
-			1, 0, 0,	//2
-            2, 0, 0,	//3
-            2, 1, 0,    //4
-            3, 1, 0     //5
+			2, 0, 0,	//1
+			3, 1, 0,	//2
+			1, 1, 0		//3
 		];
 
 		//Counter-clockwise reference of vertices
 		this.indices = [
-            0, 1, 2,
-            2, 1, 3,
-            3, 1, 4,
-            3, 4, 5,
-            2, 1, 0,
-            3, 1, 2,
-            4, 1, 3,
-            5, 4, 3	
+			2, 0, 3,
+			1, 0, 2,
+			0, 1, 2,
+			0, 2, 3
+
 		];
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
