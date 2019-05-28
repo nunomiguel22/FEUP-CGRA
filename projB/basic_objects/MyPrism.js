9555d84ecb7b3a5fa502ac527a5d1d5d3d399ff3
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2019-05-28 20:04:05 +0100

    moved basic objects to separate folder

---
+++ projB/basic_objects/MyPrism.js
@@ -0,0 +1,77 @@
+/**
+* MyPrism
+* @constructor
+*/
+class MyPrism extends CGFobject {
+    constructor(scene, slices) {
+        super(scene);
+        this.slices = slices;
+        this.initBuffers();
+    }
+
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.texCoords = [];
+
+        let alphaAng = 2*Math.PI/this.slices;
+
+        for(let i = 0; i < this.slices; i++){
+            let angle1 = i*alphaAng;
+            let x1 = Math.cos(angle1);
+            let z1 = Math.sin(angle1);
+
+            let angle2 = (i+1)*alphaAng;
+            let x2 = Math.cos(angle2);
+            let z2 = Math.sin(angle2);
+
+            this.vertices.push(x1, 0, z1);
+            this.vertices.push(x2, 0, z2);
+            this.vertices.push(x1, 1, z1);
+            this.vertices.push(x2, 1, z2);
+
+            let normal= [
+                Math.cos(angle1 + alphaAng/2),
+                0,
+                Math.sin(angle1 + alphaAng/2)
+            ];
+
+            // push normal once for each vertex of this rectangle
+            this.normals.push(
+                ...normal,
+                ...normal,
+                ...normal,
+                ...normal
+            );
+            
+            this.indices.push(
+                4*i + 1, 4*i, 4*i + 2,
+                4*i + 3, 4*i + 1, 4*i + 2
+                );
+
+            this.texCoords.push(0, 1);
+            this.texCoords.push(1, 1);
+            this.texCoords.push(0, 0);
+            this.texCoords.push(1, 0);
+        }
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+    
+    updateBuffers(complexity){
+        this.slices = 3 + Math.round(9 * complexity); //complexity leties 0-1, so slices leties 3-12
+
+        // reinitialize buffers
+        this.initBuffers();
+        this.initNormalVizBuffers();
+    }
+
+    updateTexCoords(coords) {
+		this.texCoords = [...coords];
+		this.updateTexCoordsGLBuffers();
+	}
+}
+
+
--- projB/main.js
+++ projB/main.js
@@ -2,9 +2,9 @@
 include = function () { function f() { var a = this.readyState; (!a || /ded|te/.test(a)) && (c-- , !c && e && d()) } var a = arguments, b = document, c = a.length, d = a[c - 1], e = d.call; e && c--; for (var g, h = 0; c > h; h++)g = b.createElement("script"), g.src = arguments[h], g.async = !0, g.onload = g.onerror = g.onreadystatechange = f, (b.head || b.getElementsByTagName("head")[0]).appendChild(g) };
 serialInclude = function (a) { var b = console, c = serialInclude.l; if (a.length > 0) c.splice(0, 0, a); else b.log("Done!"); if (c.length > 0) { if (c[0].length > 1) { var d = c[0].splice(0, 1); b.log("Loading " + d + "..."); include(d, function () { serialInclude([]); }); } else { var e = c[0][0]; c.splice(0, 1); e.call(); }; } else b.log("Finished."); }; serialInclude.l = new Array();
 
-serialInclude(['../lib/CGF.js','MyScene.js', 'MyInterface.js', 'Plane.js', 'MyPrism.js',
-'MyPyramid.js', 'MyQuad.js', 'MyUnitCubeQuad.js', 'MyUnitCube.js', 'MyHouse.js', 'MyCubeMap.js',
-'MyBird.js', 'MySphere.js', 'MyCone.js', 'MySquare.js', 'MyTriangle.js', 'MyBirdWing.js', 'MyTerrain.js',
+serialInclude(['../lib/CGF.js', 'MyScene.js', 'MyInterface.js', 'basic_objects/Plane.js', 'basic_objects/MyPrism.js',
+    'basic_objects/MyPyramid.js', 'basic_objects/MyQuad.js', 'basic_objects/MyUnitCubeQuad.js', 'basic_objects/MyUnitCube.js', 'MyHouse.js', 'MyCubeMap.js',
+    'MyBird.js', 'basic_objects/MySphere.js', 'basic_objects/MyCone.js', 'basic_objects/MySquare.js', 'basic_objects/MyTriangle.js', 'MyBirdWing.js', 'MyTerrain.js',
 
 
     main = function () {
